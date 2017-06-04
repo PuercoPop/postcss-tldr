13a2d7a15ea47fd1d0f4ae897c26a5afcc15fee2
Javier Olaechea <pirata@gmail.com>
2017-06-04 00:17:37 -0500

    Finish vendor-prefix plugin example

--- src/vendor-prefix.js
+++ src/vendor-prefix.js
@@ -1,9 +1,10 @@
 ;'use strict';
 var postcss = require('postcss');
 var fs = require('fs');
-var prefixes = require('./vendor-prefixes.json');
+var prefixes = require('../data/vendor-prefixes.json');
 
-var rootNode = postcss.parse(fs.readFileSync('example-2.css', { encoding: 'utf-8' }));
+
+var rootNode = postcss.parse(fs.readFileSync('./css/example-2.css', { encoding: 'utf-8' }));
 
 function declHasPrefix(decl, prefixes) {
   if (prefixes.hasOwnProperty(decl.prop)) {
@@ -13,22 +14,14 @@ function declHasPrefix(decl, prefixes) {
   }
 }
 
-// Por cada element en el arreglo prefix.prefixes va a clonar la declaration y luego remplazar el side por el elemento iterado y lo va a insertar en el padre de la decl
-function insertPrefixes(decl, prefix) {
-  prefix.prefixes.map(function (el) {
-    var newDecl = decl.clone()
-    newDecl[prefix.side] = el;
-    decl.parent.append(newDecl);
-  });
-
-}
-
 var prefixedDeclarations = []
 rootNode.walkDecls(decl => {
   var [ isPresent, prefix ] =  declHasPrefix(decl, prefixes)
   if (isPresent) {
-    prefixedDeclarations.append(insertPrefixes(decl, prefix));
-    
+    prefix.values.forEach((value) => {
+      decl.cloneBefore({value: value});
+    });
   };
 })
 
+console.log(rootNode.toString());
